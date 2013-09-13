dafa787066c1788db40ab79c22715bc71aa078ac
Doug Gerber <dojoh@outlook.com>
2013-09-13 14:29:11 -0400

    Add Model Genre

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913172840) do
+ActiveRecord::Schema.define(version: 20130913182829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20130913172840) do
     t.boolean  "available"
     t.integer  "type_id"
     t.string   "title",         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "genres", force: true do |t|
+    t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
