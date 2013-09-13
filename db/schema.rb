042d6625abb5feda845df6901f09b7fbd2cb6b43
Doug Gerber <dojoh@outlook.com>
2013-09-13 14:35:15 -0400

    Add Model Purchase

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913183254) do
+ActiveRecord::Schema.define(version: 20130913183425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,13 @@ ActiveRecord::Schema.define(version: 20130913183254) do
 
   create_table "genres", force: true do |t|
     t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "purchases", force: true do |t|
+    t.integer  "customer_id", null: false
+    t.integer  "artwork_id",  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
