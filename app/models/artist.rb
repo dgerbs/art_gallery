bae7809aee04f2caeac108956e80cc2f7d0a1832
Doug Gerber <dojoh@outlook.com>
2013-09-13 15:24:54 -0400

    Validate Email

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,7 @@ class Artist < ActiveRecord::Base
 	validates_presence_of :name
 	validates_presence_of :email
 
+	validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
 	has_many :artworks
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,8 @@ class Customer < ActiveRecord::Base
 	validates_presence_of :name
 	validates_presence_of :email
 
+	validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
 	has_many :purchases
 	has_many :artworks, through: :purchases
 
