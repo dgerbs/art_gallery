92367b48a08545242910ccd6be1fdffd5abffbf7
Doug Gerber <dojoh@outlook.com>
2013-09-13 14:32:21 -0400

    Add Model Customer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913183050) do
+ActiveRecord::Schema.define(version: 20130913183156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 20130913183050) do
   create_table "collections", force: true do |t|
     t.string   "name",       null: false
     t.text     "summary"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "customers", force: true do |t|
+    t.string   "name",       null: false
+    t.string   "email",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
